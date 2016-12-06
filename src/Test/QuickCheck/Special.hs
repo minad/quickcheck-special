1ba7b7374e3d5dda340e3fd1c2748b2ad53a6055
Daniel Mendler <mail@daniel-mendler.de>
2016-12-06 21:18:55 +0100

    also negate nan

--- src/Test/QuickCheck/Special.hs
+++ src/Test/QuickCheck/Special.hs
@@ -137,8 +137,8 @@ instance CoArbitrary a => CoArbitrary (Special a) where
   coarbitrary = coarbitrary . getSpecial
 
 specialIEEE :: FloatIEEE a => [a]
-specialIEEE = nan : list ++ map negate list
-  where list = [0, 1, epsilon, infinity, minDenormal, minNormal, maxFinite]
+specialIEEE = list ++ map negate list
+  where list = [nan, 0, 1, epsilon, infinity, minDenormal, minNormal, maxFinite]
 
 specialInt :: (Num a, Bounded a) => [a]
 specialInt = [0, 1, -1, minBound, maxBound, minBound + 1, maxBound - 1]
